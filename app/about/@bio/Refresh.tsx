cf345412b7a3f034755c9a85cfa9d356ee344024
Jonathan Mackenzie <48659911+mackbjon@users.noreply.github.com>
2023-05-21 23:36:54 -0400

    Remove server action and remove next canary

--- app/about/@bio/Refresh.tsx
+++ app/about/@bio/Refresh.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import { refreshAbout } from '../../actions'
+// import { refreshAbout } from '../../actions'
 import { useTransition } from 'react'
 
 export default function Bio() {
@@ -17,7 +17,7 @@ export default function Bio() {
           `text-sm text-gray-500 hover:text-gray-900`
         }
         // disabled={isPending}
-        onClick={() => refreshAbout()}
+        onClick={() => router.refresh()}
       >
         {/* {isPending ? 'Refreshing...' : 'Refresh'} */ 'Refresh'}
       </button>
