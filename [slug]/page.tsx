abaf78f77807fc63fda396e8c98cc36403c7c9ab
Jonathan Mackenzie <48659911+mackbjon@users.noreply.github.com>
2023-05-28 12:17:52 -0400

    remove slug

--- [slug]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function User({ params }: { params: { slug: string } }) {
-  return (
-    <>
-      <h1>{params ? `This is user # ${params}` : 'No user selected'}</h1>
-    </>
-  )
-}
